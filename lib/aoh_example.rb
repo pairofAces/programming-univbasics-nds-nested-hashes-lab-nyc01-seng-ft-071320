2b59e6842ef5caa21896dd6e27dbf272e6f19165
Karan S. Chauhan <karanchauhan16@gmail.com>
2020-06-27 15:31:15 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -25,6 +25,7 @@ def literal_aoh
     { name:  "Don Gately", occupation: "Live-in Staff" },
     { name:  "Joelle van Dyne", occupation: "Radio Personality" },
     { name:  "Pat Monteseian", occupation: "Staff" },
+    { name:  "Kate Gompert", occupation: "None" },
     
     ]
 end
